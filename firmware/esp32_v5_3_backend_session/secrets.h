105fb11b29605d89ab0daf46c61a78b4ca1e1676
Beastly713 <beastlymadness420@gmail.com>
2026-09-14 14:11:09 +0530

    chore(firmware): stop tracking local secrets

--- firmware/esp32_v5_3_backend_session/secrets.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-static const char WIFI_SSID[] = "LC";
-static const char WIFI_PASSWORD[] = "lc@tiet1";
-static const char WS_HOST[] = "objective-monitoring-backend.onrender.com";
-static const char OBJECTIVE_DEVICE_TOKEN[] = "fa3866bc84d29a8cdc68d92f4ed203e1bc42bb725332dc25389a54e1f243d457";
